c9f0b291eb9855ba51bf36d85c56591458bb30d8
dwawan <m.kurniawan@diriyanto.com>
2019-07-13 06:32:54 +0700

    Initial commit

---
+++ cst-poll.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Wordpress Poll
+ * Description: Plugin for polling a survey
+ * Plugin URI:https://www.commandmedia.net/
+ * Author: Mukh. Kurniawan
+ * Author URI: https://www.commandmedia.net/
+ * Version: 1.0.0
+ * License: GNU General Public License v2.0
+ * Text Domain: cst-poll
+ *
+ * @package cst-poll
+ */
